7513fdff058868cfa5e94dbc1b90b88b9a00c02f
piotr.minkowski <piotr.minkowski@gmail.com>
2018-06-15 11:57:13 +0200

    adding readme

--- order-service/src/main/java/pl/piomin/services/order/controller/OrderController.java
+++ order-service/src/main/java/pl/piomin/services/order/controller/OrderController.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,11 @@ public class OrderController {
 		boolean isSent = sender.send(o);
 		LOGGER.info("Order sent: {}", mapper.writeValueAsString(Collections.singletonMap("isSent", isSent)));
 		return o;
+	}
+	
+	@GetMapping("/{id}")
+	public Order findById(@PathVariable("id") Long id) {
+		return repository.findById(id);
 	}
 	
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Building and testing message-driven microservices using Spring Cloud Stream
+
+Detailed description can be found here: [Building and testing message-driven microservices using Spring Cloud Stream](https://piotrminkowski.wordpress.com/2018/06/15/building-and-testing-message-driven-microservices-using-spring-cloud-stream/) 
